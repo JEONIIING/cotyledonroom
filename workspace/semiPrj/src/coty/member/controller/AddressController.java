335f602c66e40f1468e66fbe66822a4c945efd9d
DESKTOP-92TGHOE\kimsu <jamva_98@naver.com>
2023-03-29 00:00:23 +0900

    1. 회원정보 수정 2. 주문목록 아직안함

--- workspace/semiPrj/src/coty/member/controller/AddressController.java
+++ workspace/semiPrj/src/coty/member/controller/AddressController.java
@@ -1,17 +1,26 @@
 package coty.member.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import coty.member.service.AddressListService;
+import coty.util.PageVo;
 @WebServlet("/member/address")
 public class AddressController extends HttpServlet{
+	
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/WEB-INF/views/member/address.jsp").forward(req, resp);
+		
+			
+			req.getRequestDispatcher("/WEB-INF/views/member/address.jsp").forward(req, resp);
+		
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
